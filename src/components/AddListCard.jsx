e4862151f4bab0536b91baa144eead6731fb3518
Xavier Venteo <xavierventeo@gmail.com>
2020-05-17 11:15:09 +0200

    Render form to add a card

--- src/components/AddListCard.jsx
+++ src/components/AddListCard.jsx
@@ -1,6 +1,7 @@
 import React, {useState}  from 'react';
 import './../css/ListCard.css';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 const AddListCard = (props) => {
     const [formOpen, setFormOpen] = useState(false);
@@ -14,7 +15,15 @@ const AddListCard = (props) => {
     };
 
     const renderFormAddCard = () => {
-        return 'hello';
+        return (
+            <Container>
+                <textarea class="form-control" id="newCard" rows="2"></textarea>
+                <Button variant="success" size="sm" onClick={openForm}>Añadir tarjeta</Button>
+                <button type="button" class="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </Container>
+        )
     };
 
     return formOpen ? renderFormAddCard() : renderAddButton();
