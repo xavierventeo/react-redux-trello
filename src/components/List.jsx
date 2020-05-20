02b31b6f95920b3b23ae9054e383436ace30819f
Xavier Venteo <xavierventeo@gmail.com>
2020-05-20 09:11:28 +0200

    Add draggable option

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,13 +5,23 @@ import Card from 'react-bootstrap/Card';
 import ListCard from './ListCard.jsx';
 import AddListItem from './AddListItem.jsx';
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
 const List = ( { list } ) => {
   return (
       <Card key= { list.id }>
         <Card.Header>
           <Card.Title>{ list.title }</Card.Title>
         </Card.Header>
-        <Card.Body id="cards">
+        <Card.Body id="cards" onDrop={drop} onDragOver={allowDrop}>
           { (list.cards).map( (card) => (
             <ListCard key={card.id} card={card}/>
           ))}
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './../css/ListCard.css';
 import Card from 'react-bootstrap/Card';
 
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
 const ListCard = ( { card } ) => {
   return (
-    <Card key = { card.id } id="list-card" className="bg-light">
+    <Card key = { card.id } id="list-card" className="bg-light" draggable="true" onDragStart={drag}>
       <Card.Body>{ card.text }</Card.Body>
     </Card>
   );
