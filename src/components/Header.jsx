203c02741eb44ff83f48f64bb03e533734229f00
Xavier Venteo <xavierventeo@gmail.com>
2020-05-24 12:41:46 +0200

    Styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './../css/Header.css';
+
 
 const Header = (props) => {
   return (
-    <div id="header"    >
+    <div id="header" className="header" >
         <h1>React Redux Trello</h1>
     </div>
   );
